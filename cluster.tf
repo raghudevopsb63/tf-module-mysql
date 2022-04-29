919fb44c9424ee076ef9ffbbd792bfbf4e49dee4
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-29 06:44:54 +0530

    Update terraform/tf-module-mysql/

---
+++ cluster.tf
@@ -0,0 +1,56 @@
+resource "aws_security_group" "allow_mysql" {
+  name        = "roboshop-mysql-${var.ENV}"
+  description = "roboshop-mysql-${var.ENV}"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "roboshop-redis-${var.ENV}"
+  }
+}
+
+resource "aws_db_instance" "mysql" {
+  identifier             = "roboshop-${var.ENV}"
+  allocated_storage      = 10
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t3.micro"
+  name                   = "dummy"
+  username               = "admin1"
+  password               = "RoboShop1"
+  parameter_group_name   = aws_db_parameter_group.mysql.name
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.mysql.name
+  vpc_security_group_ids = [aws_security_group.allow_mysql.id]
+}
+
+
+resource "aws_db_parameter_group" "mysql" {
+  name   = "roboshop-${var.ENV}"
+  family = "mysql5.7"
+}
+
+resource "aws_db_subnet_group" "mysql" {
+  name       = "roboshop-${var.ENV}"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+
+  tags = {
+    Name = "roboshop-${var.ENV}"
+  }
+}
+
