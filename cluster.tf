176730544776ab477edfd5b5c745f025e60a92a4
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-29 07:11:07 +0530

    Update terraform/tf-module-mysql/

--- cluster.tf
+++ cluster.tf
@@ -30,7 +30,6 @@ resource "aws_db_instance" "mysql" {
   engine                 = "mysql"
   engine_version         = "5.7"
   instance_class         = "db.t3.micro"
-  name                   = "dummy"
   username               = "admin1"
   password               = "RoboShop1"
   parameter_group_name   = aws_db_parameter_group.mysql.name
---
+++ schema.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "schema" {
+  provisioner "local-exec" {
+    command = <<EOF
+cd /tmp
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+unzip mysql.zip
+cd mysql-main
+mysql -h ${aws_db_instance.mysql.address} -u admin1 -pRoboShop1 <shipping.sql
+EOF
+  }
+}
